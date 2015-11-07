161f04c90c69540c3daad1061c1e0e057829090d
EWannerberg <daplff@gmail.com>
2015-11-07 12:50:34 +0100

    script for creating jobs for openmp added

---
+++ Assignment1/createBatchScripts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for indexVar in {1..16}
+do
+	echo -e "#!/bin/bash" \
+	"\n#@ wall_clock_limit = 00:20:00"\
+	"\n#@ job_name = pos-lulesh-openmp"\
+	"\n#@ job_type = MPICH"\
+	"\n#@ class = micro"\
+	"\n#@ output ="\
+	"\npos_lulesh_openmp_nthreads$indexVar-\$(jobid).out" \
+	"\n#@ error =" \
+	"\npos_lulesh_openmp_nthreads$indexVar-\$(jobid).out" \
+	"\n#@ node = 1" \
+	"\n#@ total_tasks = 16" \
+	"\n#@ node_usage = not_shared" \
+	"\n#@ energy_policy_tag = lulesh" \
+	"\n#@ minimize_time_to_solution = yes" \
+	"\n#@ island_count = 1" \
+	"\n#@ queue" \
+	"\n. /etc/profile" \
+	"\n. /etc/profile.d/modules.sh" \
+	"\nexport OMP_NUM_THREADS=$indexVar" \
+	"\n./lulesh2.0" > job_nthreads$indexVar.cmd
+done
