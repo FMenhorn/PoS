3cb1240108a3843f674b63520074f88fc43b8ab4
EWannerberg <daplff@gmail.com>
2015-11-07 18:08:47 +0100

    updated scripts

--- Assignment1/extractPerformanceMeasures.sh
+++ Assignment1/extractPerformanceMeasures.sh
@@ -4,7 +4,15 @@ time_line=$(grep Elapsed $1)
 grind_time_line=`grep "Grind time" $1`
 FOM_line=`grep FOM $1`
 
-result=$1
+
+jobid_tmp=${1##*jobid}
+jobid=${jobid_tmp%%-*}
+nprocs_tmp=${1##*ncpus}
+nprocs=${nprocs_tmp%%-*}
+nthreads_tmp=${1##*nthreads}
+nthreads=${nthreads_tmp%%-*}
+
+result="$jobid;$nprocs;$nthreads;"
 for string_line in "$time_line" "$grind_time_line" "$FOM_line"
 do
 
