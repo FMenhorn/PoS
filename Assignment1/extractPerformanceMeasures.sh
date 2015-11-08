fdd2ed6efed99674102ec8db89564a910c3234bc
EWannerberg <daplff@gmail.com>
2015-11-08 16:06:39 +0100

    tweak on extractPerformanceMeasures

--- Assignment1/extractPerformanceMeasures.sh
+++ Assignment1/extractPerformanceMeasures.sh
@@ -6,13 +6,16 @@ FOM_line=`grep FOM $1`
 
 
 jobid_tmp=${1##*jobid}
-jobid=${jobid_tmp%%-*}
+jobid=${jobid_tmp%%.*}
+jobid=${jobid%%-*}
+jobtype_tmp=${1##*jobtype}
+jobtype=${jobtype_tmp%%-*}
 nprocs_tmp=${1##*ncpus}
 nprocs=${nprocs_tmp%%-*}
 nthreads_tmp=${1##*nthreads}
 nthreads=${nthreads_tmp%%-*}
 
-result="$jobid;$nprocs;$nthreads;"
+result="$jobid;$jobtype;$nprocs;$nthreads;"
 for string_line in "$time_line" "$grind_time_line" "$FOM_line"
 do
 
