a5d30e06c5ccf701f92885a8f5563079dccbef2c
Benjamin Rüth <benjamin.rueth@tum.de>
2015-11-08 18:44:17 +0100

    added comment

--- Assignment1/lulesh2.0.3_PRODUCTION/loadModules.sh
+++ Assignment1/lulesh2.0.3_PRODUCTION/loadModules.sh
@@ -1,3 +1,4 @@
+echo "This bash script does not work on SuperMUC! just copy paste commands and it works."
 echo "initial module list:"
 module list
 echo "loading modules necessary for Assignment 1"
@@ -6,4 +7,3 @@ module load mpi.intel
 module load gcc/4.9
 echo "new module list:"
 module list
-
