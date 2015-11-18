79fcd3e80ade5f8c0d98261d7302f670fcc1f642
Benjamin Rüth <benjamin.rueth@tum.de>
2015-11-18 17:05:35 +0100

    put all plotting for OMP into one file

--- output/create_all_plots.m
+++ output/create_all_plots.m
@@ -1,11 +1,6 @@
 all_csv_to_mat %imports data to datastruct
 
-hgcc = plot_5_1_GCC(datastruct);
-hicc = plot_5_1_ICC(datastruct);
-title('OMP scaling')
-hl = legend([hgcc,hicc],'GCC','ICC');
-set(hl,'location','southoutside');
-
+plot_5_1(datastruct);
 plot_5_2_MPI(datastruct);
 plot_5_3_HYBRID(datastruct);
 
