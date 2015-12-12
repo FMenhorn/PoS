c0d59e4770c9e815f108a01375d80196b71f8a0e
Benjamin Rüth <benjamin.rueth@tum.de>
2015-12-12 12:21:59 +0100

    added matlab script for postproc of data + some sample data

---
+++ Assignment3/Task3/output/plot_all_data.m
@@ -0,0 +1,29 @@
+all_csv_to_mat;
+
+x = sort(unique(datastruct.size));
+y1 = zeros(numel(x),1);
+y2 = zeros(numel(x),1);
+n = zeros(numel(x),1);
+
+for i = 1:numel(datastruct.job_id)
+    j = x==datastruct.size(i);
+    y1(j) = y1(j) + datastruct.computation_time(i);
+    y2(j) = y2(j) + datastruct.MPI_time(i);
+    n(j) = n(j)+1;
+end
+
+for i = 1:numel(x)
+    y1(i) = y1(i)/n(i);
+    y2(i) = y2(i)/n(i);
+end
+
+figure(1)
+hold on
+plot(x,y1,x,y2)
+legend('computation time','MPI time')
+set(gca,'xScale','log')
+set(gca,'yScale','log')
+xlabel('problem size')
+TheLogTicks=x;
+set(gca,'XTick',TheLogTicks)
+hold off
